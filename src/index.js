e5395477b42da0543041d59cf37382f19c3da0e1
Goran <gospaso@gmail.com>
2016-01-21 13:52:38 -0500

    add(styles): separate prototype markup to components
    - begin mergin basscss markup to stylesheets

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import 'angular-ui-router';
 
 import routesConfig from './routes';
 import {GitHubService} from './app/services/github.js';
+// Containers
 import {App} from './app/containers/App';
 import {Repo} from './app/containers/Repo';
 import {User} from './app/containers/User';
+// Components
+import {Error} from './app/components/Error';
+import {Logo} from './app/components/Logo';
+import {Search} from './app/components/Search';
 
 import './index.scss';
 
@@ -15,4 +20,8 @@ angular
   .service('githubService', GitHubService)
   .component('app', App)
   .component('repo', Repo)
-  .component('user', User);
+  .component('user', User)
+  .component('error', Error)
+  .component('logo', Logo)
+  .component('search', Search)
+  ;
